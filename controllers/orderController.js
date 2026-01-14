84a28c02cd5ee7db0925a8bdff77f22c121373fd
Bwanakali254 <mwpodzoh6@gmail.com>
2026-01-14 10:39:11 +0300

    feat: connect place order flow to Pesapal
    - After creating order, submit it to Pesapal
    - Attach IPN ID when creating Pesapal order
    - Save Pesapal order_tracking_id in MongoDB
    - Return redirect_url to frontend for payment flow

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,8 @@
 import orderModel from "../models/orderModel.js";
 import productModel from "../models/productModel.js";
 import { logError, logInfo } from "../utils/logger.js";
+import { submitPesapalOrder } from "../config/pesapal.js";
+
 
 // Placing orders using pesapal
 const placeOrder = async (req, res) => {
