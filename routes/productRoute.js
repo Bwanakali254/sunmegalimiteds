affe5ff62c58e280e9be28bd7268bc1c2cf05846
Bwanakali254 <mwpodzoh6@gmail.com>
2026-01-25 15:13:43 +0300

    feat: enhance product update route to support multiple image uploads
    - Updated the product update route to allow uploading up to four images, improving flexibility for product management.
    - Maintained admin authentication for secure access to the update functionality.

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -9,6 +9,6 @@ productRouter.post('/add',adminAuth,upload.fields([{name: 'image1', maxCount: 1}
 productRouter.get('/list', listProducts);
 productRouter.post('/remove',adminAuth, removeProduct);
 productRouter.post('/single', singleProduct);
-productRouter.post('/update',adminAuth, updateProduct); 
+productRouter.post('/update', adminAuth, upload.fields([{name: 'image1', maxCount: 1},{name: 'image2', maxCount: 1},{name: 'image3', maxCount: 1},{name: 'image4', maxCount: 1}]), updateProduct);
 
 export default productRouter;
